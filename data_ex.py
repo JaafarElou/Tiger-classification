676117654d45aef54e9f7547d3f838f0e3788784
JaafarElou <jaafarelouakhchachi@gmail.com>
2024-08-31 01:20:56 +0100

    did some touches

--- data_ex.py
+++ data_ex.py
@@ -1,3 +1,3 @@
 from bing_image_downloader import downloader
 
-downloader.download("owl", limit=5, output_dir=r"tiger_data\train", adult_filter_off=False)
+downloader.download("animal", limit=25, output_dir=r"tiger_data\test\not_tiger\.", adult_filter_off=False)
